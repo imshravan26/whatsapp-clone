df30360f1d726c14ecb8862bce83c30e627a26c5
Shravan Chaudhari <shravanchaudhari99@gmail.com>
2021-04-29 20:29:45 +0530

    The authentication is completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import react from "react";
+import react, { useState } from "react";
 import "./App.css";
 import Chat from "./chat/Chat";
 import Sidebar from "./sidebar/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Login/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
+
+function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => console.log(result))
+      .catch((error) => alert(error.message));
+  };
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+          alt=""
+        />
+        <div className="login__text">
+          <h1>Sign in to Whatsapp</h1>
+        </div>
+
+        <Button onClick={signIn}>Sign in with Google</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
